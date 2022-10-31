5150de0608c2921fb6c230bccd4c604e7327a732
Preonath Shuvo <43920416+Preonath@users.noreply.github.com>
2022-10-31 11:22:43 +0600

    Update Maximum_Subarray_53.py

--- Maximum_Subarray_53.py
+++ Maximum_Subarray_53.py
@@ -1,10 +1,39 @@
+### K-mer approch ###
+
+nums=[-2,1,-3,4,-1,2,1,-5,4]
+# nums=[5,4,-1,7,8]
+def Maximum_Subarray(nums):
+    min_len_of_k_mer = 1
+    max_len_of_k_mer = len(nums)+1
+    maximum_values_of_all_k_mer=[]
+    for k in range(min_len_of_k_mer, max_len_of_k_mer):
+        res = []
+        for i in range(len(nums)-k+1):
+            k_mer = nums[i:i+k]
+            res.append(sum(k_mer))
+        all_k_mers = max(res)
+        maximum_values_of_all_k_mer.append(all_k_mers)
+    return(max(maximum_values_of_all_k_mer))
+Maximum_Subarray(nums)
+
+
 ### Dynamic problem Solution ####
-def maxSubArray(self, nums):
-    global_max, local_max = 0, 0
-    for x in nums:
-        print("Local Max: ",local_max)
-        print("Nums Value: ",x)
-        print()
-        local_max = max(0, local_max + x)
-        global_max = max(global_max, local_max)
-    return global_max
+
+## Kadane’s Algorithm ##
+
+#https://www.youtube.com/watch?v=_Nj_3FvGpZU&t=1183s #
+
+
+def Maximum_Subarray(nums):
+    if max(nums) < 0:
+        return max(nums)
+    global_maximum= nums[0]
+    current_sum=nums[0]
+
+    for i in nums[1:]:
+        current_sum = max(current_sum+i,i)
+        global_maximum=max(global_maximum,current_sum)
+    return(global_maximum)
+
+nums=[-2,1,-3,4,-1,2,1,-5,4]
+Maximum_Subarray(nums)
