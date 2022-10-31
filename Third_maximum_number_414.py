09a8a4b10982a122788c5a39e5b96b36d1e3d5f6
Preonath Shuvo <43920416+Preonath@users.noreply.github.com>
2022-10-31 12:14:22 +0600

    Update Third_maximum_number_414.py

--- Third_maximum_number_414.py
+++ Third_maximum_number_414.py
@@ -1,3 +1,19 @@
-s=input()
-m=s[::-1]
-print(m)
+def third_maximum_number(nums):
+    
+    remove_duplicate = set(nums)
+    nums = list( remove_duplicate)
+    
+    if(len(nums) == 2):
+        m = sorted(nums)
+        return(m[-1])
+    elif(len(nums) < 2):
+        return(nums[0])
+    else:
+        m = sorted(nums) 
+        return(m[-3])
+nums = [1,1,1]
+# nums = [1,2]
+# nums = [3,2,1]
+# nums = [1,2]
+# nums = [2,2,3,1]
+third_maximum_number(nums)
